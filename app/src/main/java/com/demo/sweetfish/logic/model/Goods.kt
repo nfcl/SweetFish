fd806b285b600cb5eaf443c65da49f41d2c07ec1
煖風遲來 <2872786663@qq.com>
2023-10-20 23:11:30 +0800

    为用户表添加了一个avatarBlob字段用于存储头像信息.为商品表添加了一个previewPicBlob字段用于存储预览图信息

--- app/src/main/java/com/demo/sweetfish/logic/model/Goods.kt
+++ app/src/main/java/com/demo/sweetfish/logic/model/Goods.kt
@@ -1,16 +1,51 @@
 package com.demo.sweetfish.logic.model
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.ByteArrayOutputStream
+import java.sql.Blob
 
 @Entity
 data class Goods(
     var title: String,
     var price: Double,
-    var sellerId: Long
+    var sellerId: Long,
+    private var previewPicBlob: Blob
 ) {
 
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
 
+    /**
+     * 获得预览图片
+     * @return 返回一个[Drawable]作为图片对象
+     */
+    fun getPreviewPic(): Drawable? {
+        return Drawable.createFromStream(previewPicBlob.binaryStream, "img")
+    }
+
+    /**
+     * 设置预览图片
+     * @param drawable 设置的头像图片
+     */
+    fun setPreviewPic(drawable: Drawable?) {
+        if (drawable == null) {
+            return
+        }
+        val bitmap = Bitmap
+            .createBitmap(
+                drawable.intrinsicWidth,
+                drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+        drawable.draw(canvas)
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        previewPicBlob.setBytes(0, stream.toByteArray())
+    }
 }
--- app/src/main/java/com/demo/sweetfish/logic/model/User.kt
+++ app/src/main/java/com/demo/sweetfish/logic/model/User.kt
@@ -1,7 +1,13 @@
 package com.demo.sweetfish.logic.model
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.ByteArrayOutputStream
+import java.sql.Blob
+
 
 /**
  * 用户类
@@ -12,14 +18,46 @@ import androidx.room.PrimaryKey
  * @property sex        性别
  * @property account    账号
  * @property password   密码
+ * @property avatarBlob 头像的二进制数据
  */
 @Entity
 data class User(
     var name: String,
     var sex: Boolean,
     var account: String,
-    var password: String
+    var password: String,
+    private var avatarBlob: Blob
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
+
+    /**
+     * 获得头像图片
+     * @return 返回一个[Drawable]作为图片对象
+     */
+    fun getAvatar(): Drawable? {
+        return Drawable.createFromStream(avatarBlob.binaryStream, "img")
+    }
+
+    /**
+     * 设置头像图片
+     * @param drawable 设置的头像图片
+     */
+    fun setAvatar(drawable: Drawable?) {
+        if (drawable == null) {
+            return
+        }
+        val bitmap = Bitmap
+            .createBitmap(
+                drawable.intrinsicWidth,
+                drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+        drawable.draw(canvas)
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        avatarBlob.setBytes(0, stream.toByteArray())
+    }
 }
