6ccc22c943b7a2c4e429469e501a8db1078512f1
nfcl <2872786663@qq.com>
2023-10-20 14:55:57 +0800

    忘记加注释了

--- app/src/main/java/com/demo/poshmarkinc/logic/model/User.kt
+++ app/src/main/java/com/demo/poshmarkinc/logic/model/User.kt
@@ -7,9 +7,11 @@ import androidx.room.PrimaryKey
  * 用户类
  *
  * 用于保存用户信息
- * @property id     自动生成的唯一标识符
- * @property name   昵称
- * @property sex    性别
+ * @property id         唯一标识符
+ * @property name       昵称
+ * @property sex        性别
+ * @property account    账号
+ * @property password   密码
  */
 @Entity
 data class User(
