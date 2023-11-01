bc0599186b984d2935a7afc47d0fbf9d0b347da4
煖風遲來 <2872786663@qq.com>
2023-11-01 22:02:22 +0800

    1.更改了主页面的搜索框部分,使其只是单独作为搜索页面的跳板 2.使用ViewModel为主页面的商品推荐列表提供数据

--- app/src/main/java/com/demo/sweetfish/ui/homePage/HomePageActivity.kt
+++ app/src/main/java/com/demo/sweetfish/ui/homePage/HomePageActivity.kt
@@ -2,38 +2,30 @@ package com.demo.sweetfish.ui.homePage
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.demo.sweetfish.AppDatabase
-import com.demo.sweetfish.logic.model.Goods
-import com.demo.sweetfish.logic.model.GoodsWithSellerInfo
-import com.demo.sweetfish.logic.model.User
+import androidx.lifecycle.ViewModelProvider
 import com.demo.sweetfish.ui.goodsPublishPage.GoodsPublishPageActivity
+import com.demo.sweetfish.ui.searchPage.SearchPageActivity
 import com.demo.sweetfish.ui.tradePage.MyBoughtActivity
 import com.demo.sweetfish.ui.tradePage.MyPublishActivity
 import com.demo.sweetfish.ui.tradePage.MySoldActivity
 import com.demo.sweetfish.ui.userPage.personal.PersonalUserPageActivity
 import com.example.sweetfish.R
-import org.jetbrains.annotations.TestOnly
-import utils.DrawableUtils
-import utils.DrawableUtils.Companion.toBytes
-import java.lang.ref.WeakReference
-import java.time.Instant
-import kotlin.concurrent.thread
-import kotlin.math.abs
-import kotlin.random.Random
 
 class HomePageActivity : AppCompatActivity() {
+
+    private val viewModel: HomePageActivityViewModel by lazy {
+        ViewModelProvider(
+            this, HomePageActivityViewModel.HomePageActivityViewModelFactory()
+        )[HomePageActivityViewModel::class.java]
+    }
 
     companion object {
         const val HOME_PAGE: Int = 0
         const val USER_PAGE: Int = 1
-        const val GOODSLISTINIT: Int = 0x0001
     }
 
     private lateinit var viewPager: NoScrollViewPager
@@ -43,33 +35,6 @@ class HomePageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home_page)
         initViewPager()
         initNavigation()
-    }
-
-    private val weakHandler by lazy { WeakReferenceHandler(this) }
-
-    class WeakReferenceHandler(obj: HomePageActivity) : Handler(Looper.getMainLooper()) {
-        private val mRef: WeakReference<HomePageActivity> = WeakReference(obj)
-
-        override fun handleMessage(msg: Message) {
-            mRef.get()?.run {
-                when (msg.what) {
-                    GOODSLISTINIT -> {
-                        val goodsList: HomePageGoodsList =
-                            homePageView.findViewById(R.id.HomePageGoodsList)
-                        val goodsData = msg.obj as List<GoodsWithSellerInfo>
-                        goodsList.setGoodsList(
-                            goodsData
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        //退出页面时，置空所以的Message
-        weakHandler.removeCallbacksAndMessages(null)
-        super.onDestroy()
     }
 
     private fun initNavigation() {
@@ -100,19 +65,11 @@ class HomePageActivity : AppCompatActivity() {
         viewPager.adapter = HomePageViewPagerAdapter(viewList)
     }
 
-    private lateinit var homePageView: View
-
     private fun initHomePage(homePageView: View) {
-        fun initGoodsList() {
-            thread {
-                val msg: Message = Message()
-                msg.what = GOODSLISTINIT
-                msg.obj = AppDatabase.getDatabase().goodsWithSellerInfoDao().findAll()
-                weakHandler.sendMessage(msg)
-            }
-        }
-        this.homePageView = homePageView
-        initGoodsList()
+        val goodsList: HomePageGoodsList = homePageView.findViewById(R.id.HomePageGoodsList)
+        viewModel.goodsList.observe(this) { listData -> goodsList.setGoodsList(listData) }
+        val searchBar: LinearLayout = findViewById(R.id.HomePageSearchBar)
+        searchBar.setOnClickListener { startActivity(Intent(this, SearchPageActivity::class.java)) }
     }
 
     private fun initUserPage(userPageView: View) {
@@ -134,71 +91,71 @@ class HomePageActivity : AppCompatActivity() {
             }
     }
 
-    /**
-     * 测试用数据生成
-     */
-    @TestOnly
-    private fun listTest() {
-        fun userInit(num: Int): List<User> {
-            val result = ArrayList<User>()
-            val random = Random(Instant.now().epochSecond)
-            for (i in 1..num) {
-                result.add(
-                    User(
-                        "用户$i",
-                        random.nextBoolean(),
-                        "123456",
-                        "123456",
-                        DrawableUtils.getGradientDrawable(
-                            100, 100, intArrayOf(
-                                random.nextInt() % 0xFFFFFF, random.nextInt() % 0xFFFFFF
-                            )
-                        ).toBytes()
-                    )
-                )
-            }
-            return result
-        }
-
-        fun goodsInit(num: Int, users: List<User>): List<Goods> {
-            val result = ArrayList<Goods>()
-            val random = Random(Instant.now().epochSecond)
-            for (i in 1..num) {
-                result.add(
-                    Goods(
-                        "商品名称$i",
-                        (((random.nextDouble() * 100).toInt()) % 1000).toDouble() / 100,
-                        users[abs(random.nextInt()) % users.size].id,
-                        DrawableUtils.getGradientDrawable(
-                            abs(random.nextInt()) % 100 + 500,
-                            abs(random.nextInt()) % 100 + 500,
-                            intArrayOf(
-                                random.nextInt() % 0xFFFFFF, random.nextInt() % 0xFFFFFF
-                            )
-                        ).toBytes()
-                    )
-                )
-            }
-            return result
-        }
-        thread {
-            val userDao = AppDatabase.getDatabase().userDao()
-            val goodsDao = AppDatabase.getDatabase().goodsDao()
-
-            userDao.deleteAll()
-            goodsDao.deleteAll()
-
-            val userList = userInit(10)
-
-            for (data in userList) {
-                data.id = userDao.insert(data)
-            }
-
-            val goodsList = goodsInit(20, userList)
-
-            for (data in goodsList) {
-                data.id = goodsDao.insert(data)
-            }
-        }
-    }
+//    /**
+//     * 测试用数据生成
+//     */
+//    @TestOnly
+//    private fun listTest() {
+//        fun userInit(num: Int): List<User> {
+//            val result = ArrayList<User>()
+//            val random = Random(Instant.now().epochSecond)
+//            for (i in 1..num) {
+//                result.add(
+//                    User(
+//                        "用户$i",
+//                        random.nextBoolean(),
+//                        "123456",
+//                        "123456",
+//                        DrawableUtils.getGradientDrawable(
+//                            100, 100, intArrayOf(
+//                                random.nextInt() % 0xFFFFFF, random.nextInt() % 0xFFFFFF
+//                            )
+//                        ).toBytes()
+//                    )
+//                )
+//            }
+//            return result
+//        }
+//
+//        fun goodsInit(num: Int, users: List<User>): List<Goods> {
+//            val result = ArrayList<Goods>()
+//            val random = Random(Instant.now().epochSecond)
+//            for (i in 1..num) {
+//                result.add(
+//                    Goods(
+//                        "商品名称$i",
+//                        (((random.nextDouble() * 100).toInt()) % 1000).toDouble() / 100,
+//                        users[abs(random.nextInt()) % users.size].id,
+//                        DrawableUtils.getGradientDrawable(
+//                            abs(random.nextInt()) % 100 + 500,
+//                            abs(random.nextInt()) % 100 + 500,
+//                            intArrayOf(
+//                                random.nextInt() % 0xFFFFFF, random.nextInt() % 0xFFFFFF
+//                            )
+//                        ).toBytes()
+//                    )
+//                )
+//            }
+//            return result
+//        }
+//        thread {
+//            val userDao = AppDatabase.getDatabase().userDao()
+//            val goodsDao = AppDatabase.getDatabase().goodsDao()
+//
+//            userDao.deleteAll()
+//            goodsDao.deleteAll()
+//
+//            val userList = userInit(10)
+//
+//            for (data in userList) {
+//                data.id = userDao.insert(data)
+//            }
+//
+//            val goodsList = goodsInit(20, userList)
+//
+//            for (data in goodsList) {
+//                data.id = goodsDao.insert(data)
+//            }
+//        }
+//    }
 }
---
+++ app/src/main/java/com/demo/sweetfish/ui/homePage/HomePageActivityViewModel.kt
@@ -0,0 +1,36 @@
+package com.demo.sweetfish.ui.homePage
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.demo.sweetfish.AppDatabase
+import com.demo.sweetfish.SweetFishApplication
+import com.demo.sweetfish.logic.model.GoodsWithSellerInfo
+import kotlin.concurrent.thread
+
+class HomePageActivityViewModel : ViewModel() {
+
+    private val _goodsList: MutableLiveData<List<GoodsWithSellerInfo>> =
+        MutableLiveData<List<GoodsWithSellerInfo>>()
+
+    val goodsList: LiveData<List<GoodsWithSellerInfo>>
+        get() = _goodsList
+
+    init {
+
+        thread {
+            val goodsWithSellerInfoDao =
+                AppDatabase.getDatabase(SweetFishApplication.context).goodsWithSellerInfoDao()
+            _goodsList.postValue(goodsWithSellerInfoDao.findAll())
+        }
+
+    }
+
+    class HomePageActivityViewModelFactory() : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return HomePageActivityViewModel() as T
+        }
+    }
+}
+
