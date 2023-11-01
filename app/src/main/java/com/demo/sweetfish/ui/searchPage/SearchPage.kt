c87f48b560864c502dcc82225b7c7e09dfa31871
煖風遲來 <2872786663@qq.com>
2023-11-01 16:14:17 +0800

    搜索页面

---
+++ app/src/main/java/com/demo/sweetfish/ui/searchPage/SearchPage.kt
@@ -0,0 +1,12 @@
+package com.demo.sweetfish.ui.searchPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sweetfish.R
+
+class SearchPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_page)
+    }
+}
