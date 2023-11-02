15fb9d4718331fdb95968c70c65dc88f80949e64
煖風遲來 <2872786663@qq.com>
2023-11-02 21:44:55 +0800

    修改了获得Resources中的Color的方法 Resources.getColor -> ContextCompat.getColor 前者已经被弃用

--- app/src/main/java/com/demo/sweetfish/ui/userPage/UserPageTagLayoutItem.kt
+++ app/src/main/java/com/demo/sweetfish/ui/userPage/UserPageTagLayoutItem.kt
@@ -3,19 +3,36 @@ package com.demo.sweetfish.ui.userPage
 import android.content.Context
 import android.content.res.ColorStateList
 import android.util.AttributeSet
+import androidx.core.content.ContextCompat
+import com.demo.sweetfish.SweetFishApplication
 import com.example.sweetfish.R
 
 class UserPageTagLayoutItem(context: Context, attrs: AttributeSet) :
     androidx.appcompat.widget.AppCompatTextView(context, attrs) {
 
     fun unSelect() {
-        this.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.UserPageTagLayoutItemUnSelectBackground))
-        this.setTextColor(resources.getColor(R.color.UserPageTagLayoutItemUnSelectText))
+        this.backgroundTintList = ColorStateList.valueOf(
+            ContextCompat.getColor(
+                SweetFishApplication.context, R.color.UserPageTagLayoutItemUnSelectBackground
+            )
+        )
+        this.setTextColor(
+            ContextCompat.getColor(
+                SweetFishApplication.context, R.color.UserPageTagLayoutItemUnSelectText
+            )
+        )
     }
 
     fun select() {
-        this.backgroundTintList =
-            ColorStateList.valueOf(resources.getColor(R.color.UserPageTagLayoutItemSelectBackground))
-        this.setTextColor(resources.getColor(R.color.UserPageTagLayoutItemSelectText))
+        this.backgroundTintList = ColorStateList.valueOf(
+            ContextCompat.getColor(
+                SweetFishApplication.context, R.color.UserPageTagLayoutItemSelectBackground
+            )
+        )
+        this.setTextColor(
+            ContextCompat.getColor(
+                SweetFishApplication.context, R.color.UserPageTagLayoutItemSelectText
+            )
+        )
     }
 }
