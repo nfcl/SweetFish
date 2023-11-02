95933f21a22a79ea2d05da6c972a3e4cd94251a9
煖風遲來 <2872786663@qq.com>
2023-11-02 21:49:04 +0800

    添加了查找与提供的商品名称近似的商品的方法

--- app/src/main/java/com/demo/sweetfish/logic/dao/GoodsWithSellerInfoDao.kt
+++ app/src/main/java/com/demo/sweetfish/logic/dao/GoodsWithSellerInfoDao.kt
@@ -37,4 +37,20 @@ interface GoodsWithSellerInfoDao {
     )
     fun findByGoodsIds(id: LongArray): List<GoodsWithSellerInfo>
 
+    @Query(
+        """ select 
+                Goods.id as goodsId,
+                Goods.title as goodsTitle,
+                Goods.price as goodsPrice,
+                Goods.previewPic as goodsPreviewPic,
+                User.id as sellerId,
+                User.name as sellerName,
+                User.avatarPic as sellerAvatarPic 
+            from 
+                Goods inner join User on Goods.sellerId=User.id 
+            where 
+                Goods.title like (:goodsName)"""
+    )
+    fun findLikeGoodsName(goodsName: String): List<GoodsWithSellerInfo>
+
 }
