f129bcbe368768da143ce63f40df394aa3efdc0e
煖風遲來 <2872786663@qq.com>
2023-11-08 19:06:04 +0800

    完善了用户信息修改界面的逻辑

--- app/src/main/java/com/demo/sweetfish/ui/userPage/userInfoEdit/UserInfoEditPageActivity.kt
+++ app/src/main/java/com/demo/sweetfish/ui/userPage/userInfoEdit/UserInfoEditPageActivity.kt
@@ -3,6 +3,7 @@ package com.demo.sweetfish.ui.userPage.userInfoEdit
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -10,8 +11,16 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.demo.sweetfish.SweetFishApplication
 import com.demo.sweetfish.ui.component.RoundImageView
 import com.example.sweetfish.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.lljjcoder.Interface.OnCityItemClickListener
+import com.lljjcoder.bean.CityBean
+import com.lljjcoder.bean.DistrictBean
+import com.lljjcoder.bean.ProvinceBean
+import com.lljjcoder.citywheel.CityConfig
+import com.lljjcoder.style.citypickerview.CityPickerView
 import utils.DrawableUtils
 import kotlin.concurrent.thread
 
@@ -23,24 +32,135 @@ class UserInfoEditPageActivity : AppCompatActivity() {
         )[UserInfoEditPageActivityViewModel::class.java]
     }
 
+    private val mPicker = CityPickerView()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info_edit_page)
         initComponent()
+        initObserve()
     }
 
-    private fun initComponent() {
-        findViewById<ImageView>(R.id.UserInfoEditPageReturnButton).setOnClickListener {
-            finish()
-        }
+    private fun initObserve() {
         val avatarImageView: RoundImageView = findViewById(R.id.UserInfoEditPageAvatarImageView)
         val idTextView: TextView = findViewById(R.id.UserInfoEditPageIdTextView)
         val sexTextView: TextView = findViewById(R.id.UserInfoEditPageSexTextView)
         val nameTextView: TextView = findViewById(R.id.UserInfoEditPageNameTextView)
+        val positionTextView: TextView = findViewById(R.id.UserInfoEditPagePositionTextView)
+        val describeTextView: TextView = findViewById(R.id.UserInfoEditPageDescribeTextView)
+        val backgroundImageView: RoundImageView =
+            findViewById(R.id.UserInfoEditPageBackgroundImageView)
         viewModel.userId.observe(this) { id -> idTextView.text = id.toString() }
-        viewModel.userName.observe(this) { name -> sexTextView.text = name }
-        viewModel.userSex.observe(this) { sex -> nameTextView.text = sex }
+        viewModel.userName.observe(this) { name -> nameTextView.text = name }
+        viewModel.userSex.observe(this) { sex ->
+            sexTextView.text = when (sex) {
+                null -> "选择性别"
+                true -> "男"
+                false -> "女"
+            }
+        }
         viewModel.userAvatar.observe(this) { avatar -> avatarImageView.setImageDrawable(avatar) }
+        viewModel.userPosition.observe(this) { position ->
+            positionTextView.text = if (position == null) {
+                "选择你所在的地区"
+            } else {
+                "${position.province} ${position.city} ${position.district}"
+            }
+        }
+        viewModel.userDescribe.observe(this) { describe ->
+            describeTextView.text = describe ?: "真诚的简介可以让买卖更有信任"
+        }
+        viewModel.userBackground.observe(this) { background ->
+            backgroundImageView.setImageDrawable(
+                background
+            )
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun initComponent() {
+        //返回按钮
+        findViewById<ImageView>(R.id.UserInfoEditPageReturnButton).setOnClickListener {
+            finish()
+        }
+        //名称更改
+        findViewById<RelativeLayout>(R.id.UserInfoEditPageNameEditButton).setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(this)
+            bottomSheetDialog.setContentView(
+                LayoutInflater.from(SweetFishApplication.context)
+                    .inflate(R.layout.activity_user_info_edit_page_name_edit_bottom_dialog, null)
+            )
+            val diaLogCancelButton =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogCancelButton)!!
+            val diaLogConfirmButton =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogConfirmButton)!!
+            val diaLogContentEditText =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogContentEditText)!!
+            diaLogCancelButton.setOnClickListener {
+                bottomSheetDialog.hide()
+            }
+            diaLogConfirmButton.setOnClickListener {
+                thread {
+                    viewModel.setName(diaLogContentEditText.text.toString())
+                    runOnUiThread {
+                        bottomSheetDialog.hide()
+                    }
+                }
+            }
+            diaLogContentEditText.text = viewModel.userName.value
+            bottomSheetDialog.show()
+        }
+        //性别选择
+        findViewById<RelativeLayout>(R.id.UserInfoEditPageSexEditButton).setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(this)
+            bottomSheetDialog.setContentView(
+                LayoutInflater.from(SweetFishApplication.context)
+                    .inflate(R.layout.activity_user_info_edit_page_sex_edit_bottom_dialog, null)
+            )
+            val diaLogCancelButton =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogCancelButton)!!
+            val diaLogConfirmButton =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogConfirmButton)!!
+            val diaLogMaleTextView =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogSexMaleTextView)!!
+            val diaLogFeMaleTextView =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogSexFeMaleTextView)!!
+            diaLogCancelButton.setOnClickListener {
+                bottomSheetDialog.hide()
+            }
+            diaLogConfirmButton.setOnClickListener {
+                thread {
+                    viewModel.setSex(viewModel.sexDialogSelect)
+                    runOnUiThread {
+                        bottomSheetDialog.hide()
+                    }
+                }
+            }
+            fun selectMale() {
+                diaLogMaleTextView.setTextColor(getColor(R.color.UserInfoEditPageSexDialogSelectTextColor))
+                diaLogFeMaleTextView.setTextColor(getColor(R.color.UserInfoEditPageSexDialogUnSelectTextColor))
+                viewModel.sexDialogSelect = true
+            }
+
+            fun selectFemale() {
+                diaLogMaleTextView.setTextColor(getColor(R.color.UserInfoEditPageSexDialogUnSelectTextColor))
+                diaLogFeMaleTextView.setTextColor(getColor(R.color.UserInfoEditPageSexDialogSelectTextColor))
+                viewModel.sexDialogSelect = false
+            }
+            diaLogMaleTextView.setOnClickListener {
+                selectMale()
+            }
+            diaLogFeMaleTextView.setOnClickListener {
+                selectFemale()
+            }
+            if (viewModel.userSex.value == null || viewModel.userSex.value == true) {
+                selectMale()
+            } else {
+                selectFemale()
+            }
+            bottomSheetDialog.show()
+        }
+        //头像选择
         val avatarSelectLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) { result ->
@@ -61,6 +181,122 @@ class UserInfoEditPageActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_PICK)
             intent.type = "image/*"
             avatarSelectLauncher.launch(intent)
+        }
+        //地区选择
+        mPicker.init(this)
+        val cityConfig = CityConfig.Builder()
+            //标题
+            .title("")
+            //标题文字大小
+            .titleTextSize(18)
+            //标题文字颜色
+            .titleTextColor("#585858")
+            //标题栏背景色
+            .titleBackgroundColor(getColor(R.color.primary).toHexString())
+            //确认按钮文字颜色
+            .confirTextColor("#000000")
+            //确认按钮文字
+            .confirmText("确定")
+            //确认按钮文字大小
+            .confirmTextSize(20)
+            //取消按钮文字颜色
+            .cancelTextColor("#000000")
+            //取消按钮文字
+            .cancelText("取消")
+            //取消按钮文字大小
+            .cancelTextSize(20)
+            //显示类，只显示省份一级，显示省市两级还是显示省市区三级
+            .setCityWheelType(CityConfig.WheelType.PRO_CITY_DIS)
+            //是否显示半透明背景
+            .showBackground(true)
+            //显示item的数量
+            .visibleItemsCount(5)
+            //默认显示的省份
+            .province("浙江省")
+            //默认显示省份下面的城市
+            .city("杭州市")
+            //默认显示省市下面的区县数据
+            .district("临安区")
+            //省份滚轮是否可以循环滚动
+            .provinceCyclic(false)
+            //城市滚轮是否可以循环滚动
+            .cityCyclic(false)
+            //区县滚轮是否循环滚动
+            .districtCyclic(false)
+            //滚轮显示模糊效果
+            .drawShadows(true)
+            //中间横线的颜色
+            .setLineColor(getColor(R.color.primary).toHexString())
+            //中间横线的高度
+            .setLineHeigh(5)
+            //是否显示港澳台数据，默认不显示
+            .setShowGAT(true).build()
+        mPicker.setConfig(cityConfig)
+        mPicker.setOnCityItemClickListener(object : OnCityItemClickListener() {
+            override fun onSelected(
+                province: ProvinceBean?,
+                city: CityBean?,
+                district: DistrictBean?,
+            ) {
+                thread {
+                    viewModel.setPosition(province.toString(), city.toString(), district.toString())
+                }
+            }
+        })
+        findViewById<RelativeLayout>(R.id.UserInfoEditPagePositionEditButton).setOnClickListener {
+            mPicker.showCityPicker()
+        }
+        //描述更改
+        findViewById<RelativeLayout>(R.id.UserInfoEditPageDescribleEditButton).setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(this)
+            bottomSheetDialog.setContentView(
+                LayoutInflater.from(SweetFishApplication.context)
+                    .inflate(
+                        R.layout.activity_user_info_edit_page_describe_edit_bottom_dialog,
+                        null
+                    )
+            )
+            val diaLogCancelButton =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogCancelButton)!!
+            val diaLogConfirmButton =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogConfirmButton)!!
+            val diaLogContentEditText =
+                bottomSheetDialog.findViewById<TextView>(R.id.UserInfoEditPageNameEditDialogContentEditText)!!
+            diaLogCancelButton.setOnClickListener {
+                bottomSheetDialog.hide()
+            }
+            diaLogConfirmButton.setOnClickListener {
+                thread {
+                    viewModel.setDescribe(diaLogContentEditText.text.toString())
+                    runOnUiThread {
+                        bottomSheetDialog.hide()
+                    }
+                }
+            }
+            diaLogContentEditText.text = viewModel.userDescribe.value
+            bottomSheetDialog.show()
+        }
+        //背景选择
+        val backgroundSelectLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val currentUri: Uri = result.data?.data!!
+                thread {
+                    val inputStream = contentResolver.openInputStream(currentUri)!!
+                    viewModel.setBackground(
+                        DrawableUtils.createDrawableFromByteArray(
+                            inputStream.readBytes(), ""
+                        )
+                    )
+                    inputStream.close()
+                }
+            }
+        }
+        findViewById<RelativeLayout>(R.id.UserInfoEditPageHomePageBackgroundEditButton).setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            backgroundSelectLauncher.launch(intent)
         }
     }
 }
--- app/src/main/java/com/demo/sweetfish/ui/userPage/userInfoEdit/UserInfoEditPageActivityViewModel.kt
+++ app/src/main/java/com/demo/sweetfish/ui/userPage/userInfoEdit/UserInfoEditPageActivityViewModel.kt
@@ -8,36 +8,75 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.demo.sweetfish.AppDatabase
 import com.demo.sweetfish.SweetFishApplication
+import com.demo.sweetfish.logic.model.UserPositionInfo
 
 class UserInfoEditPageActivityViewModel : ViewModel() {
 
-    val userId: LiveData<String> =
+    val userId: LiveData<Long> = Transformations.map(SweetFishApplication.loginUser) {
+        it.id
+    }
+    val userName: LiveData<String> = Transformations.map(SweetFishApplication.loginUser) {
+        it.name ?: "填写专属昵称，更容易被大家记住"
+    }
+    val userSex: LiveData<Boolean?> = Transformations.map(SweetFishApplication.loginUser) {
+        it.sex
+    }
+    val userAvatar: LiveData<Drawable> = Transformations.map(SweetFishApplication.loginUser) {
+        it.avatarPic
+    }
+    var sexDialogSelect = true
+    val userBackground: LiveData<Drawable> = Transformations.map(SweetFishApplication.loginUser) {
+        it.background
+    }
+    val userDescribe: LiveData<String?> = Transformations.map(SweetFishApplication.loginUser) {
+        it.describe
+    }
+    val userPosition: LiveData<UserPositionInfo?> =
         Transformations.map(SweetFishApplication.loginUser) {
-            it.id.toString()
-        }
-    val userName: LiveData<String> =
-        Transformations.map(SweetFishApplication.loginUser) {
-            it.name ?: "填写专属昵称，更容易被大家记住"
-        }
-    val userSex: LiveData<String> =
-        Transformations.map(SweetFishApplication.loginUser) {
-            when (it.sex) {
-                null -> "选择性别"
-                true -> "男"
-                false -> "女"
-            }
-        }
-    val userAvatar: LiveData<Drawable> =
-        Transformations.map(SweetFishApplication.loginUser) {
-            it.avatarPic
+            it.position
         }
 
     @WorkerThread
     fun setAvatar(drawable: Drawable) {
-        val newUserInfo = SweetFishApplication.loginUser.value!!
-        newUserInfo.avatarPic = drawable
-        val userDao = AppDatabase.getDatabase().userDao()
-        userDao.update(newUserInfo)
+        AppDatabase.getDatabase().userDao()
+            .updateAvatar(SweetFishApplication.loginUser.value!!.id, drawable)
+        SweetFishApplication.forceRefreshUserInfo()
+    }
+
+    @WorkerThread
+    fun setName(name: String) {
+        AppDatabase.getDatabase().userDao()
+            .updateName(SweetFishApplication.loginUser.value!!.id, name)
+        SweetFishApplication.forceRefreshUserInfo()
+    }
+
+    @WorkerThread
+    fun setSex(sex: Boolean?) {
+        AppDatabase.getDatabase().userDao()
+            .updateSex(SweetFishApplication.loginUser.value!!.id, sex)
+        SweetFishApplication.forceRefreshUserInfo()
+    }
+
+    @WorkerThread
+    fun setPosition(province: String, city: String, district: String) {
+        AppDatabase.getDatabase().userDao().updatePosition(
+            SweetFishApplication.loginUser.value!!.id, UserPositionInfo(province, city, district)
+        )
+        SweetFishApplication.forceRefreshUserInfo()
+    }
+
+    @WorkerThread
+    fun setDescribe(describe: String) {
+        AppDatabase.getDatabase().userDao().updateDescribe(
+            SweetFishApplication.loginUser.value!!.id, describe
+        )
+        SweetFishApplication.forceRefreshUserInfo()
+    }
+
+    @WorkerThread
+    fun setBackground(drawable: Drawable) {
+        AppDatabase.getDatabase().userDao()
+            .updateBackground(SweetFishApplication.loginUser.value!!.id, drawable)
         SweetFishApplication.forceRefreshUserInfo()
     }
 
