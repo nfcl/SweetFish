0189fe7422638c8507f787fc7537991bd0ccb812
煖風遲來 <2872786663@qq.com>
2023-11-08 19:01:33 +0800

    添加了CityPicker组件用于省市区的地址选择，github仓库为https://github.com/crazyandcoder/citypicker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.room:room-common:2.1.0")
     implementation("androidx.room:room-ktx:2.1.0")
+    implementation("com.github.crazyandcoder:citypicker:6.0.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     kapt("androidx.room:room-compiler:2.1.0")
     testImplementation("junit:junit:4.13.2")
