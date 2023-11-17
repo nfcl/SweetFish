e2de3dbd772398b8db441c12c2d7c26048e3aa26
煖風遲來 <2872786663@qq.com>
2023-11-17 10:20:22 +0800

    由于发布商品功能还未增加添加预览图功能，因此将预览图改成Nullable

--- app/src/main/java/com/demo/sweetfish/logic/model/GoodsWithSellerInfo.kt
+++ app/src/main/java/com/demo/sweetfish/logic/model/GoodsWithSellerInfo.kt
@@ -6,7 +6,7 @@ data class GoodsWithSellerInfo(
     val goodsId: Long,
     val goodsTitle: String,
     val goodsPrice: Double,
-    val goodsPreviewPic: Drawable,
+    val goodsPreviewPic: Drawable?, //TODO 由于发布商品功能还未增加添加预览图功能，因此将预览图改成Nullable
     val sellerId: Long,
     val sellerName: String?,
     val sellerAvatarPic: Drawable,
